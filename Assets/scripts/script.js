53d8286da03061511d907fd1242b7387681b5398
Buse Hizarci <bhzrc2705@gmail.com>
2021-10-17 23:12:13 +0100

    Main script added

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,1 +1,11 @@
-﻿
+﻿var btn_accordion = document.getElementById("btn-accordion");
+
+btn_accordion.addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  })
